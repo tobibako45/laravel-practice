24d992e835479c83e99741bd9cc7a6457dfb5cb2
tibibako45 <tobibako45@gmail.com>
2019-05-19 12:35:15 +0900

    Add global scope

--- database/seeds/PeopleTableSeeder.php
+++ database/seeds/PeopleTableSeeder.php
@@ -21,14 +21,14 @@ class PeopleTableSeeder extends Seeder
         $param = [
             'name' => 'hanako',
             'mail' => 'hanako@flower.jp',
-            'age' => 12,
+            'age' => 34,
         ];
         DB::table('people')->insert($param);
 
         $param = [
             'name' => 'sachiko',
             'mail' => 'sachiko@happy.jp',
-            'age' => 12,
+            'age' => 56,
         ];
         DB::table('people')->insert($param);
 
